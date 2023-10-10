32aa7eceaf36496519c222dc39ce382bec7e5bd1
Everton Fernande <evertonfrnds@gmail.com>
2023-10-10 14:32:02 -0300

    update impl

--- src/schema/extractSchemaValues.ts
+++ src/schema/extractSchemaValues.ts
@@ -7,18 +7,21 @@ export const extractSchemaValues = (schemaPayload: any) => {
     const routeRegex = /<@route>(.*?)<\/@route>/;
     const bodyRegex = /<@body>(.*?)<\/@body>/;
     const queryRegex = /<@query>(.*?)<\/@query>/;
+    const controllerRegex = /<@controller>(.*?)<\/@controller>/;
 
     const keyMatch = line.match(keyRegex);
     const reqMatch = line.match(reqRegex);
     const routeMatch = line.match(routeRegex);
     const bodyMatch = line.match(bodyRegex);
     const queryMatch = line.match(queryRegex);
+    const controllerMatch = line.match(controllerRegex);
 
     const keyValue = keyMatch ? keyMatch[1] : null;
     const reqValue = reqMatch ? reqMatch[1] : null;
     const routeValue = routeMatch ? routeMatch[1] : null;
     const bodyValue = bodyMatch ? bodyMatch[1] : null;
     const queryValue = queryMatch ? queryMatch[1] : null;
+    const controllerValue = controllerMatch ? controllerMatch[1] : null;
 
     return {
       key: keyValue,
@@ -26,8 +29,27 @@ export const extractSchemaValues = (schemaPayload: any) => {
       route: routeValue,
       body: bodyValue,
       query: queryValue,
+      controller: controllerValue,
     };
   });
 
   return fields;
+};
+
+export const getControllerKeyValue = (schema: any) => {
+  const mappedSchema = schema.reduce((acc: any, crr: any) => {
+    if (acc[crr.controller]) {
+      return {
+        ...acc,
+        [crr.controller]: [...acc[crr.controller], crr],
+      };
+    } else {
+      return {
+        ...acc,
+        [crr.controller]: [crr],
+      };
+    }
+  }, {});
+
+  return mappedSchema;
 };
--- src/tasks/generateImplTask.ts
+++ src/tasks/generateImplTask.ts
@@ -2,6 +2,8 @@ import { ListrTask } from "listr2";
 import { Config } from "../config";
 import { writeFileSync } from "fs";
 import { join } from "path";
+import { render } from "ejs";
+import { getControllerKeyValue } from "../schema/extractSchemaValues";
 
 export const generateImplTask = (config: Config): ListrTask => {
   const processSchema = async (
@@ -12,60 +14,76 @@ export const generateImplTask = (config: Config): ListrTask => {
     const { path } = targetConfig.output;
 
     let renderData = `import * as t from './types';` + "\n\n";
+    renderData += `import { AxiosInstance } from "axios";` + "\n\n";
+
     renderData +=
-      `import axios from 'axios';
+      `export const getFunctions = (axiosInstance: AxiosInstance) => {` + "\n";
 
-export const axiosInstance = axios.create({
-  baseURL: '${targetConfig.input.axios.baseUrl}',  
-});` + "\n\n";
+    const mappedController = getControllerKeyValue(schema);
+    Object.keys(mappedController).forEach((controllerName) => {
+      const items = mappedController[controllerName];
 
-    schema.forEach((item) => {
-      const hasArgs = mappedTypesArgs.find((arg) => arg.key === item.key);
+      renderData += `  const ${controllerName} = {` + "\n";
 
-      let func =
-        `export async function ${item.key}(${
-          hasArgs ? `args: t.${hasArgs.name}` : ""
-        }) {` + "\n";
+      items.forEach((item: any) => {
+        const hasArgs = mappedTypesArgs.find((arg) => arg.key === item.key);
 
-      func += `  let path = "${item.route}"` + "\n";
+        let func =
+          `    async ${item.key}(${
+            hasArgs ? `args: t.${hasArgs.name}` : ""
+          }) {` + "\n";
 
-      const paramRegex = /:([^/]+)/g;
-      const paramMatch = paramRegex.test(item.route);
+        func += `      let path = "${item.route}"` + "\n";
 
-      const param = paramMatch
-        ? `
-  const params = args.params;
+        const paramRegex = /:([^/]+)/g;
+        const paramMatch = paramRegex.test(item.route);
 
-  for (const paramKey in params) {
-    if (params.hasOwnProperty(paramKey)) {
-      const value = params[paramKey];
-      path = path.replace(\`:\${paramKey}\`, value.toString());
-   }
-  }
-    `
-        : "";
+        const param = paramMatch
+          ? `
+      const params = args.params;
+          
+      for (const paramKey in params) {
+        if (params.hasOwnProperty(paramKey)) {
+          const value = params[paramKey];
+          path = path.replace(\`:\${paramKey}\`, value.toString());
+       }
+      }
+      `
+          : "";
 
-      const bodyMatch = item.body ? `args.body` : `null`;
-      const queryMatch = item.query ? `args.query` : `null`;
+        const bodyMatch = item.body ? `args.body` : `null`;
+        const queryMatch = item.query ? `args.query` : `null`;
 
-      func += param;
+        func += param;
 
-      func += `
-  try {
-    const response = await axiosInstance({
-      method: '${String(item.req).toLowerCase()}',
-      data: ${bodyMatch},
-      params: ${queryMatch},
-      url: path,
+        func += `
+      try {
+        const response = await axiosInstance({
+          method: '${String(item.req).toLowerCase()}',
+          data: ${bodyMatch},
+          params: ${queryMatch},
+          url: path,
+        });
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+    },`;
+
+        renderData += func + "\n";
+      });
+      renderData += `  };\n`;
     });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}`;
 
-      renderData += func + "\n\n";
+    renderData += `  return {\n`;
+
+    Object.keys(mappedController).forEach((controllerName) => {
+      renderData += `    ${controllerName},\n`;
     });
+
+    renderData += `  };\n`;
+
+    renderData += `};` + "\n";
 
     writeFileSync(join(process.cwd(), path, "impl.ts"), renderData);
   };
